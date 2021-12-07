3dff913423879f9038f08ae9706eee28c1660655
Anil Kumar <amehla105@gmail.com>
2021-12-07 17:12:04 +0530

    student java file committed

---
+++ Student.java
@@ -0,0 +1,20 @@
+class Stclass
+{
+Sting Sname,SBranch;
+int Srollno,Smarks;
+
+Stclass(int r,Sting sn,Sting sb,int sm)
+{
+Sname=sn;
+SBranch=sb;
+Srollno=sr;
+Smarks=sm;
+} 
+public void display()
+{
+
+System.out.ptintln(Sname+" "+Smarks+" "+Srollno+" "+Smarks);
+
+}
+
+}
